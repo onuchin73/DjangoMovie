4f3abf541f455f8435fe24558070aaef8d575b21
Onuchin Anton <qwerty4774@yandex.ru>
2021-08-26 19:51:38 +0300

    new

--- DjangoMovie/movies/urls.py
+++ DjangoMovie/movies/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.MoviesVies.as_view())
+    path("", views.MoviesVies.as_view()),
+    path("<int:pk>/", views.MovieDetailView.as_view())
 ]
